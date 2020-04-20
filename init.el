39fb3c6d3552fb009055a7804479cf632cb9baca
guo libao <guo.libao@aztech.com>
2020-04-20 21:54:11 +0800

    initial commit from dot-emacs-dot-d-for-windows

---
+++ init.el
@@ -0,0 +1,33 @@
+(setq package-check-signature nil)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("ELPA"  . "https://tromey.com/elpa/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
+			 ("melpa" . "https://melpa.milkbox.net/packages/")
+			 ))
+
+(package-initialize)
+
+;;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smex htmlize plantuml-mode highlight-symbol auctex use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
