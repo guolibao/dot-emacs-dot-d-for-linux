df2f3e7a6e4a3dff2a11e0fd650b3fa429f3b5bf
guo libao <guo.libao@aztech.com>
2020-04-21 15:24:55 +0800

    add magit and replace "" to <>

--- init.el
+++ init.el
@@ -24,7 +24,14 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck-clang-analyzer flycheck-irony irony-eldoc company-irony company-c-headers flycheck company smex htmlize plantuml-mode highlight-symbol auctex use-package))))
+    (magit flycheck-clang-analyzer flycheck-irony irony-eldoc company-irony company-c-headers flycheck company smex htmlize plantuml-mode highlight-symbol auctex use-package)))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+	   (setq company-c-headers-path-user
+		 (delete-dups
+		  (append company-c-headers-path-user
+			  (list "/home/aztech/works/aztech_sps100/1113driver/knx" "/home/aztech/works/aztech_sps100/1113driver/knx/common" "/home/aztech/works/aztech_sps100/1113driver/knx/control" "/home/aztech/works/aztech_sps100/1113driver/knx/dlist" "/home/aztech/works/aztech_sps100/1113driver/knx/HAL" "/home/aztech/works/aztech_sps100/1113driver/knx/init" "/home/aztech/works/aztech_sps100/1113driver/knx/init/cfg" "/home/aztech/works/aztech_sps100/1113driver/knx/parse" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/cEMI" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/cEMI/AL" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/cEMI/DLL" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/cEMI/NL" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/cEMI/TL" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/filter" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/GA" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/HID" "/home/aztech/works/aztech_sps100/1113driver/knx/parse/TPH" "/home/aztech/works/aztech_sps100/1113driver/knx/receive" "/home/aztech/works/aztech_sps100/1113driver/knx/store")))))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
